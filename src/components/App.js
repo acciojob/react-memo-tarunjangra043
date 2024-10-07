0e47c8e8f65e52cec7a1c1fdeb9487d7a808af14
Tarun Jangra <tarunjangra043@gmail.com>
2024-10-07 11:44:43 +0530

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import UseMemo from "./UseMemo";
 import ReactMemo from "./ReactMemo";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [customTask, setCustomTask] = useState("");
-
-  const addTodo = () => {
-    setTodos([...todos, "New todo"]);
-  };
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleCustomTaskChange = (e) => {
-    setCustomTask(e.target.value);
-  };
-
-  const submitCustomTask = () => {
-    if (customTask.length > 5) {
-      setTodos([...todos, customTask]);
-      setCustomTask("");
-    } else {
-      alert("Task must be more than 5 characters!");
-    }
-  };
-
   return (
-    <div>
-      <h1>Todo List</h1>
-      <button onClick={addTodo}>Add Todo</button>
-      <button onClick={incrementCounter}>Increment Counter</button>
-      <div>
-        <input
-          type="text"
-          value={customTask}
-          onChange={handleCustomTaskChange}
-          placeholder="Enter custom task (more than 5 characters)"
-        />
-        <button onClick={submitCustomTask}>Submit</button>
-      </div>
-      <div>
-        <h2>Todos:</h2>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2>Counter: {counter}</h2>
-      </div>
-      <UseMemo todos={todos} />
+    <div id="main">
+      <h1>React.useMemo</h1>
+
+      <UseMemo />
+
       <ReactMemo />
     </div>
   );
--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ReactMemo = React.memo(() => {
-  console.log("Rendering ReactMemo component");
-  return <div>This component is memoized!</div>;
-});
+const ReactMemo = () => {
+  const [skills, setSkills] = useState([]);
+  const [skillInput, setSkillInput] = useState("");
+  return (
+    <div>
+      <h1>React.memo</h1>
+      <input
+        id="skill-input"
+        type="text"
+        onChange={(e) => setSkillInput(e.target.value)}
+        value={skillInput}
+      />
+      <button id="skill-btn" onClick={() => setSkills([...skills, skillInput])}>
+        Add Skill
+      </button>
+      <ul id="item-jumbotron">
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ReactMemo;
